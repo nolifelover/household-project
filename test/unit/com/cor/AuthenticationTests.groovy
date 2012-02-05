3110f34484ad1205424076ffe2662c043a41b786
Todsaporn Sangboon <todsaporn@foobarsystem.com>
2012-02-06 01:38:58 +0700

    adding authentication

---
+++ test/unit/com/cor/AuthenticationTests.groovy
@@ -0,0 +1,17 @@
+package com.cor
+
+import grails.test.*
+
+class AuthenticationTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/household/project/LoginFiltersTests.groovy
@@ -0,0 +1,17 @@
+package household.project
+
+import grails.test.*
+
+class LoginFiltersTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
